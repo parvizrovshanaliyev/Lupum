020b8a92993184e2c669706ba2c01836134f19b1
parvizrovshanaliyev <parvizra@code.edu.az>
2019-04-08 11:44:29 +0400

    admin panel view ready

---
+++ Lupum_Yolcu/Migrations/201904080651287_user_table_change3.cs
@@ -0,0 +1,16 @@
+namespace Lupum_Yolcu.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class user_table_change3 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
