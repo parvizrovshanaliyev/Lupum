e9a09a6f086f5645e45b2c6017c1eef260f6c60b
parvizrovshanaliyev <parvizra@code.edu.az>
2019-04-08 09:02:39 +0400

    Add project files.

---
+++ Lupum_Yolcu/DAL/LupumContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Lupum_Yolcu.DAL
+{
+    public class LupumContext:DbContext
+    {
+        public LupumContext():base("LupumStr")
+        {
+
+        }
+    }
+}
---
+++ Lupum_Yolcu/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lupum_Yolcu.MvcApplication" Language="C#" %>
